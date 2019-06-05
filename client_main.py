7b34c56c126174a70c17418e5d46b7e05cb80bef
kilichenko <glebk98@gmail.com>
2019-06-05 12:46:40 +0300

    client

--- client_main.py
+++ client_main.py
@@ -1,29 +1,19 @@
 from app_client import Client
 import time
-import threading
 
-sender, receiver = 'gleb', 'ivan'
+sender, receiver = 'ivan', 'gleb'
 content = ''
 action = 'message'
 
 
 def main():
-    getter = threading.Thread(target=getter_thread)
-    getter.run()
     msg = ''
     while msg != 'q':
         msg = input('Input message: ')
         client = Client()
         client.run(action=action, content=msg, sender=sender, receiver=receiver)
-
-
-def getter_thread():
-    while True:
-        time.sleep(2)
         client = Client()
         client.run(action='getnewmsgs', sender=sender)
-
-
 
 def get_sender():
     return sender
